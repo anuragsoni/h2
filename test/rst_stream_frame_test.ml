fd57496707e01d9c84fa8b6326149ac5b4397508
Anurag Soni <anuragsoni.13@gmail.com>
2018-08-30 22:44:30 -0400

    add missing test

--- test/rst_stream_frame_test.ml
+++ test/rst_stream_frame_test.ml
@@ -15,7 +15,7 @@ let parse_rst_frame () =
   Alcotest.(check int) "Length" 4 parsed.frame_header.length ;
   Alcotest.(check int) "flags" 0 parsed.frame_header.flags ;
   Alcotest.(check int) "stream id" 5 parsed.frame_header.stream_id ;
-  Alcotest.(check int) "type" 3 (frame_type_of_id parsed.frame_header.frame_type);
+  Alcotest.(check int) "type" 3 (frame_type_of_id parsed.frame_header.frame_type) ;
   Alcotest.(check int) "Error code" 8 (error_code_of_id error)
 
 let parse_rst_frame' () =
@@ -23,9 +23,10 @@ let parse_rst_frame' () =
   let error = extract_payload parsed.frame_payload in
   Alcotest.(check int) "Length" 4 parsed.frame_header.length ;
   Alcotest.(check int) "flags" 0 parsed.frame_header.flags ;
-  Alcotest.(check int) "stream id" 5 parsed.frame_header.stream_id ;
-  Alcotest.(check int) "type" 3 (frame_type_of_id parsed.frame_header.frame_type);
+  Alcotest.(check int) "stream id" 1 parsed.frame_header.stream_id ;
+  Alcotest.(check int) "type" 3 (frame_type_of_id parsed.frame_header.frame_type) ;
   Alcotest.(check int) "Error code" 420 (error_code_of_id error)
 
-
-let tests = [("Can parse rst stream header", `Quick, parse_rst_frame)]
+let tests =
+  [ ("Can parse rst stream header", `Quick, parse_rst_frame)
+  ; ("Second rst frame test", `Quick, parse_rst_frame') ]
