3d66636c124196ad443e88d765cdfe52f292fd2b
Anurag Soni <anuragsoni.13@gmail.com>
2018-08-30 20:32:10 -0400

    add test for rst stream

--- test/frames_test.ml
+++ test/frames_test.ml
@@ -1,4 +1,5 @@
 let () =
   Alcotest.run "Frames tests"
     [ ("Dataframe tests", Data_frame_test.tests)
-    ; ("Priority frame tests", Priority_frame_test.tests) ]
+    ; ("Priority frame tests", Priority_frame_test.tests)
+    ; ("RSTStream frame tests", Rst_stream_frame_test.tests) ]
