cd4c0c6dca366ee5b33f0f20dd4196c5427404e0
Anurag Soni <anurag@sonianurag.com>
2018-09-03 13:33:37 -0400

    Add more serializers (#20)
    * Serialize ping frame
    
    * Serialize go away frame
    
    * Serialize window frame

--- test/frames_test.ml
+++ test/frames_test.ml
@@ -4,4 +4,7 @@ let () =
     ; ("Priority frame tests", Priority_frame_test.tests)
     ; ("RSTStream frame tests", Rst_stream_frame_test.tests)
     ; ("Settings frame tests", Settings_frame_test.tests)
-    ; "Push promise frame tests", Push_promise_frame_test.tests]
+    ; ("Push promise frame tests", Push_promise_frame_test.tests)
+    ; ("Ping frame test", Ping_frame_test.tests)
+    ; ("GoAway frame test", Go_away_frame_test.tests)
+    ; ("Window frame test", Window_frame_test.tests) ]
---
+++ test/ping_frame_test.ml
@@ -0,0 +1,12 @@
+open Frames
+
+let wire = "0000080600000000006465616462656566"
+
+let serialize_ping_frame () =
+  let f = Faraday.create 8 in
+  let info = {Serialize.flags = 0; stream_id = 0; padding = None} in
+  Frames.Serialize.write_frame f info (Types.PingFrame "deadbeef") ;
+  let output = Faraday.serialize_to_string f in
+  Alcotest.(check string) "serialize" wire (Util.hex_of_string output)
+
+let tests = [("Serialize ping frame", `Quick, serialize_ping_frame)]
