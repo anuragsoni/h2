ae0433bc9d7719f168b770236d5066f2d831caba
Anurag Soni <anuragsoni.13@gmail.com>
2018-09-18 21:33:51 -0400

    refactor

--- h2/lib/pqueue.ml
+++ h2/lib/pqueue.ml
@@ -1,36 +1,85 @@
-module type Comparable = sig
-  type t
+open Frames
 
+type deficit = int
+
+let deficit_steps = 65536
+
+let deficit_table =
+  Array.init 256 (fun i ->
+      let open Base in
+      Float.to_int
+        (Float.round (Float.of_int deficit_steps /. Float.of_int (i + 1))) )
+
+let deficit_of_weight w = deficit_table.(w - 1)
+
+type precedence =
+  {deficit : deficit; weight : Types.weight; dependency : Types.stream_id}
+
+let precedence_of_priority {Types.stream_dependency; weight; _} =
+  {deficit = 0; weight; dependency = stream_dependency}
+
+let compare_precedence {deficit = d1; _} {deficit = d2; _} =
+  compare (d1 : int) d2
+
+(** TODO: remove me. used for test *)
+let new_p w = {deficit = 0; weight = w; dependency = 0l}
+
+module Make (V : sig
   type v
-
-  val compare : t -> t -> int
-end
-
-module Make (K : Map.OrderedType) (V : Comparable) = struct
+end) =
+struct
   module Q =
     Psq.Make
-      (K)
+      (Int32)
       (struct
-        type t = V.t * V.v
+        type t = precedence * V.v
 
-        let compare (t1, _) (t2, _) = V.compare t1 t2
+        let compare (t1, _) (t2, _) = compare_precedence t1 t2
       end)
 
-  type t = Q.t
+  type t = {base_deficit : deficit; queue : Q.t}
 
-  let empty = Q.empty
+  let empty = {base_deficit = 0; queue = Q.empty}
 
-  let is_empty = Q.is_empty
+  let is_empty {queue = q; _} = Q.is_empty q
 
-  let add k p v q = Q.add k (p, v) q
+  let add k ({weight; deficit; _} as p) v ({base_deficit; queue} as q) =
+    let d = deficit_of_weight weight in
+    let b = if deficit = 0 then base_deficit else deficit in
+    let deficit' = max (b + d) base_deficit in
+    let p' = {p with deficit = deficit'} in
+    let queue' = Q.add k (p', v) queue in
+    {q with queue = queue'}
 
-  let pop q =
-    match Q.pop q with
+  let pop {queue; _} =
+    match Q.pop queue with
     | None -> None
-    | Some ((k, (p, v)), q') -> Some (k, p, v, q')
+    | Some ((k, (p, v)), q') ->
+        let base_deficit = p.deficit in
+        Some (k, p, v, {base_deficit; queue = q'})
 
-  let remove k q = Q.remove k q
+  let remove k q =
+    let f = Q.find k q.queue in
+    match f with
+    | None -> (None, q)
+    | Some (_, v) -> (
+      match Q.min q.queue with
+      | None -> failwith "can't find min value"
+      | Some (k', (p', _)) ->
+          if k' = k then
+            (* We removed the min element. Update base_deficit just like we do in
+               [pop] *)
+            (Some v, {base_deficit = p'.deficit; queue = Q.remove k q.queue})
+          else (Some v, {q with queue = Q.remove k q.queue}) )
 
-  let find k q = Q.find k q
-
+  (* TODO: (remove me) used for testing *)
+  let enqdeq queue num =
+    let rec loop queue num acc =
+      if num = 0 then acc
+      else
+        match pop queue with
+        | None -> failwith "enqdeq"
+        | Some (k, p, v, q') -> loop (add k p v q') (num - 1) (k :: acc)
+    in
+    loop queue num []
 end
--- h2/lib/priority.ml
+++ h2/lib/priority.ml
@@ -1,68 +1,45 @@
 open Base
 open Frames
 
-type key = int32
+module rec PriorityTree : sig
+  type t
 
-type deficit = int
+  module Q : sig
+    type t
+  end
 
-let deficit_steps = 65536
+  val empty : t
+end = struct
+  module Q = Pqueue.Make (struct
+    type v = Node.v
+  end)
 
-let deficit_table =
-  Array.init 256 ~f:(fun i ->
-      Float.to_int
-        (Float.round (Float.of_int deficit_steps /. Float.of_int (i + 1))) )
+  type t =
+    { nodes : (int32, Pqueue.precedence, Int32.comparator_witness) Map.t
+    ; root : Node.v }
 
-let deficit_of_weight w = deficit_table.(w - 1)
-
-type precedence =
-  {deficit : deficit; weight : Types.weight; dependency : Types.stream_id}
-
-let compare {deficit = d1; _} {deficit = d2; _} = Int.compare d1 d2
-
-(** TODO: remove me. used for test *)
-let new_p w = {deficit = 0; weight = w; dependency = 0l}
-
-module PriorityQueue = struct
-  module Q =
-    Pqueue.Make
-      (Int32)
-      (struct
-        type t = precedence
-
-        type v = int32
-
-        let compare = compare
-      end)
-
-  type t = {base_deficit : deficit; queue : Q.t}
-
-  let empty = {base_deficit = 0; queue = Q.empty}
-
-  let is_empty {queue = q; _} = Q.is_empty q
-
-  let add k ({weight; deficit; _} as p) v ({base_deficit; queue} as q) =
-    let d = deficit_of_weight weight in
-    let b = if deficit = 0 then base_deficit else deficit in
-    let deficit' = Int.max (b + d) base_deficit in
-    let p' = {p with deficit = deficit'} in
-    let queue' = Q.add k p' v queue in
-    {q with queue = queue'}
-
-  let pop {queue; _} =
-    match Q.pop queue with
-    | None -> None
-    | Some (k, p, v, queue') ->
-        let base_deficit = p.deficit in
-        Some (k, p, v, {base_deficit; queue = queue'})
-
-  (** TODO: remove me. used for test *)
-  let enqdeq queue num =
-    let rec loop queue num acc =
-      if num = 0 then acc
-      else
-        match pop queue with
-        | None -> failwith "enqdeq"
-        | Some (k, p, v, q') -> loop (add k p v q') (num - 1) (k :: acc)
+  let empty =
+    let m = Map.empty (module Int32) in
+    let nodes =
+      Map.add_exn m ~key:0l
+        ~data:(Pqueue.precedence_of_priority Types.default_priority)
     in
-    loop queue num []
+    let root =
+      { Node.id = 0l
+      ; precedence = Pqueue.precedence_of_priority Types.default_priority
+      ; children = Q.empty }
+    in
+    {nodes; root}
+end
+
+and Node : sig
+  type v =
+    { id : Types.stream_id
+    ; precedence : Pqueue.precedence
+    ; children : PriorityTree.Q.t }
+end = struct
+  type v =
+    { id : Types.stream_id
+    ; precedence : Pqueue.precedence
+    ; children : PriorityTree.Q.t }
 end
